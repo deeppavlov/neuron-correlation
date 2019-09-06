0d938af1d4b3813a7a675b2a014baf825ce95dfb
Anton Peganov <peganov@phystech.edu>
2019-09-06 19:12:55 +0300

    refactor: rename main function and move it to api.py

---
+++ neuron_correlation/api.py
@@ -0,0 +1,12 @@
+from typing import Dict
+
+from tensorflow.examples.tutorials.mnist import input_data
+
+
+def conduct_train_experiment(config: Dict):
+    """Launch training and computing of correlation
+
+    Args:
+        config: Dict with configuration of experiment
+    """
+    pass
