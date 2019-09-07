49c81c5d784bd357c624a3a5eaaeba4dce944bb2
PeganovAnton <peganoff2@mail.ru>
2019-09-07 23:18:50 +0300

    remove conduct_train_experiment unit test

--- tests/unit/test_api.py
+++ tests/unit/test_api.py
@@ -3,25 +3,3 @@ from unittest import mock
 import pytest
 
 from neuron_correlation import api
-
-
-class TestConductTrainExperiment:
-    def test_process_start_join_calls(self):
-        config = {
-            "num_repeats": 7,
-            "graph": {},
-            "train": {},
-            "test": {}
-        }
-        with mock.patch('multiprocessing.Process') as MockProcess, \
-                mock.patch('multiprocessing.Queue'), \
-                mock.patch('neuron_correlation.api.calculate_mean_and_std'):
-            api.conduct_train_experiment(config)
-            start_call_count = MockProcess.return_value.start.call_count
-            join_call_count = MockProcess.return_value.join.call_count
-            assert start_call_count == config['num_repeats'], \
-                "mp.Process.start() was called {} times whereas {} calls were expected".format(
-                    start_call_count, config['num_repeats'])
-            assert join_call_count == config['num_repeats'], \
-                "mp.Process.join() was called {} times whereas {} calls were expected".format(
-                    join_call_count, config['num_repeats'])
